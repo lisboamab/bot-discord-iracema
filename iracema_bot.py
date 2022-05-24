dcf721d9d94569c73cc149a8a30c2ec57d07fa7e
lisboamab <lisboamab@gmail.com>
2022-05-24 19:21:33 -0300

    adicionada a funcionalidade de cotação de moedas

--- iracema_bot.py
+++ iracema_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from key_bot.token_bot import token
+import requests
 
 
 bot = commands.Bot("$")
@@ -31,6 +32,24 @@ async def hello_model(ctx):
     response = (f"Olá {name}!!!")
     await ctx.send(response)
 
+@bot.command() #mostra a cotação de uma moeda em relação ao real
+async def cotacao(ctx, coin):
+    try:
+        resposta = requests.get(f"https://economia.awesomeapi.com.br/json/last/{coin.upper()}-BRL")
+        data = resposta.json() # {"USDBRL":{"code":"USD","codein":"BRL","name":"Dólar Americano/Real Brasileiro","high":"4.8536","low":"4.776","varBid":"0.0075","pctChange":"0.16","bid":"4.819","ask":"4.822","timestamp":"1653424057","create_date":"2022-05-24 17:27:37"}}
+        fulljson = data.get(f"{coin}BRL")
+        high = float(fulljson.get(f"high"))
+        low = float(fulljson.get("low"))
+        median_price = (high + low)/2
+
+        if median_price:
+            await ctx.send(f"O valor do {coin} é {median_price:.2f} reais")
+        else:
+            await ctx.send(f"O valor {coin} é invalido")
+
+    
+    except:
+        await ctx.send("Ops ... Deu algum erro!")
 
 @bot.command(name="apresentação")
 async def presentention_model(ctx):
@@ -40,7 +59,7 @@ async def presentention_model(ctx):
     await ctx.send(presentation)
 
 
-@bot.command(name="convite")
+@bot.command(name="convite") #faz com que o bot envie o link de convite do servidor no chat
 async def presentention_model(ctx):
     link_convite = "https://discord.gg/Y7T5Fegc7u"
     mensagem = (f'Convide seus amigos: {link_convite}')
